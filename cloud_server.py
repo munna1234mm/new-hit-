84238d88d72e288b9e388adbddf95978290d26ce
munna1234mm <munna1234mm@gmail.com>
2026-04-03 00:15:04 -0600

    Added CORS support

--- cloud_server.py
+++ cloud_server.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify, render_template_string
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
+CORS(app) # This allows the extension to talk to the server
 
 # In-memory storage for the current task and results
 task_data = {
